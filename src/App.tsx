b2645b26a65efa3f7e05e01894087ab407b12d70
Eman Radwan <emanalaa11499@gmail.com>
2025-11-27 20:38:52 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import FilterTabs from "./components/FilterTabs";
 import MenuList from "./components/MenuList";
-import Search from "./components/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container mx-auto py-10 px-3 md:px-5">
-      <Search />
-      <main className="flex flex-col gap-y-10">
-        <FilterTabs />
-        <MenuList />
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mx-auto pt-28 pb-10 px-3 md:px-5">
+        <main className="flex flex-col gap-y-10">
+          <FilterTabs />
+          <MenuList />
+        </main>
+      </div>
+    </>
   );
 }
 
