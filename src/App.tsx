09e199a41e9c1b7666f491ec91c151682549ebb2
Eman Radwan <emanalaa11499@gmail.com>
2025-11-27 17:31:42 +0200

    dispay items in cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import MenuList from "./components/ui/MenuList";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container mx-auto py-20 px-5">
+      <main>
+        <MenuList />
+      </main>
+    </div>
+  );
 }
 
 export default App;
