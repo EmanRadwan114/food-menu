300190644a54e31059547cca8e2760da5b6f6427
Eman Radwan <emanalaa11499@gmail.com>
2025-11-27 23:01:43 +0200

    add not found text

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -18,11 +18,17 @@ const MenuList: React.FC = () => {
 
   return (
     <AnimatePresence>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 xl:w-5/6 mx-auto">
-        {meals.map((item) => (
-          <MenuCard item={item} key={item.id} />
-        ))}
-      </div>
+      {meals.length ? (
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 xl:w-5/6 mx-auto">
+          {meals.map((item) => (
+            <MenuCard item={item} key={item.id} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-orange-700 text-xl text-center mt-5 font-semibold">
+          No Meals Found
+        </p>
+      )}
     </AnimatePresence>
   );
 };
